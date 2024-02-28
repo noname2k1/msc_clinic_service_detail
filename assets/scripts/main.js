4c14fd5d37456f747c7e5e9ccf5bc1b10571a437
ninhnam <ninhnamyb2001@gmail.com>
2024-02-28 22:52:29 +0700

    some bugs lỏ

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,19 +5,21 @@ const elements = document.querySelectorAll(
 elements.forEach((element) => {
     element.onfocus = function (e) {
         if (!e.target.value) {
-            e.target.nextElementSibling.style.display = 'none';
+            e.target.nextElementSibling.style.visibility = 'hidden';
         }
+        e.target.parentNode.style.border = 'unset';
+        e.target.parentNode.querySelector('span.error').textContent = '';
     };
     element.onblur = function (e) {
         if (!e.target.value) {
-            e.target.nextElementSibling.style.display = 'block';
+            e.target.nextElementSibling.style.visibility = 'visible';
         }
     };
     element.onchange = function (e) {
         if (e.target.value) {
-            e.target.nextElementSibling.style.display = 'none';
+            e.target.nextElementSibling.style.visibility = 'hidden';
         } else {
-            e.target.nextElementSibling.style.display = 'block';
+            e.target.nextElementSibling.style.visibility = 'visible';
         }
     };
 });
@@ -54,9 +56,78 @@ button.onclick = function (e) {
 };
 
 // go to top
-document.querySelector('li.go-to-top').onclick = function () {
+const goToTopBtn = document.querySelector('li.go-to-top');
+goToTopBtn.onclick = function () {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
+};
+
+let prevScrollPosition = document.documentElement.scrollTop;
+let myElement = document.querySelector('.header-wrapper');
+goToTopBtn.style.display = 'none';
+window.addEventListener('scroll', function () {
+    let currentScrollPosition = document.documentElement.scrollTop;
+
+    if (currentScrollPosition < prevScrollPosition) {
+        // Người dùng đang cuộn lên trên
+        myElement.style.transform = 'translateY(0)';
+    } else {
+        // Người dùng đang cuộn xuống dưới
+        myElement.style.transform = 'translateY(-100%)';
+    }
+    if (currentScrollPosition < 1000) {
+        goToTopBtn.style.display = 'none';
+    } else {
+        goToTopBtn.style.display = 'flex';
+    }
+
+    // Cập nhật lại vị trí cuộn trước
+    prevScrollPosition = currentScrollPosition;
+});
+
+//responsive
+let mm = gsap.matchMedia();
+
+// gsap
+gsap.registerPlugin(ScrollTrigger);
+mm.add('(min-width: 768px)', () => {
+    gsap.to('.fourth-section', {
+        scrollTrigger: {
+            trigger: '.fourth-section',
+            pin: true, // pin the trigger element while active
+            start: 'top top', // when the top of the trigger hits the top of the viewport
+            end: '+=500', // end after scrolling 500px beyond the start
+            scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+            snap: {
+                snapTo: 'labels', // snap to the closest label in the timeline
+                duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+                ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+            }
+        }
+    });
+});
+
+var box1 = document.querySelector('.fourth-section .overlay');
+let targets = gsap.utils.toArray('.scroll-with-effect li');
+
+// validate input required
+const orderBtn = document.querySelector('#order-btn');
+orderBtn.onclick = function (e) {
+    e.preventDefault();
+    const inputs = Array.from(
+        e.target.closest('form').querySelectorAll('input,textarea')
+    );
+
+    const requiredInputName = ['name', 'phone'];
+    for (let i = 0; i < inputs.length; i++) {
+        if (requiredInputName.includes(inputs[i].name) && !inputs[i].value) {
+            inputs[i].parentNode.querySelector('.error').textContent =
+                'Vui lòng điền đủ thông tin cần thiết';
+            inputs[i].closest('.input-group').style.border = '1px solid red';
+            break;
+        }
+    }
 };
